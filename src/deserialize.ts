be14a7e69dd495a442c0996ad015597f4b04a6c3
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2017-10-26 10:30:04 +0200

    feat(all) initial commit

---
+++ src/deserialize.ts
@@ -0,0 +1,89 @@
+import { JsonMetadata, METADATA_KEY } from './json-metadata';
+
+export function deserialize<T>(type: { new(): T; }, source: any): T {
+    if (source === undefined) {
+        return undefined;
+    }
+    if (source === null) {
+        return null;
+    }
+    let destination = new type();
+
+    for (let key in destination) {
+        let propertyMetadata: JsonMetadata<T> = getJsonPropertyMetadata<T>(destination, key);
+        if (propertyMetadata) {
+            destination[key] = getValue(source, destination, key, propertyMetadata);
+        } else {
+            if (source[key] !== undefined) {
+                destination[key] = source[key];
+            }
+        }
+    }
+
+    return destination;
+}
+
+const isPrimitive = object => {
+    switch (typeof object) {
+        case "string":
+        case "number":
+        case "boolean":
+            return true;
+    }
+    return !!(
+        object instanceof String || object === String ||
+        object instanceof Number || object === Number ||
+        object instanceof Boolean || object === Boolean
+    );
+}
+
+const isArray = object => {
+    if (object === Array) {
+        return true;
+    } else if (typeof Array.isArray === "function") {
+        return Array.isArray(object);
+    }
+    else {
+        return !!(object instanceof Array);
+    }
+}
+
+const getJsonPropertyMetadata = <T>(target: any, propertyKey: string): JsonMetadata<T> =>
+    Reflect.getOwnMetadata(METADATA_KEY, Object.getPrototypeOf(target), propertyKey);
+
+const getValue = <T>(source: any, destination: T, key: string, propertyMetadata: JsonMetadata<T>) => {
+    let propertyName = propertyMetadata.name || key;
+    let type = getType(destination, key);
+
+    if (isArray(type)) {
+        if (propertyMetadata.ctor) {
+            if (isArray(source[propertyName])) {
+                return source[propertyName].map(item => propertyMetadata.ctor(item));
+            }
+            else {
+                return propertyMetadata.ctor(source[propertyName]);
+            }
+        }
+        else if (propertyMetadata.type) {
+            if (isArray(source[propertyName])) {
+                return source[propertyName].map(item => deserialize(propertyMetadata.type, item));
+            }
+            else {
+                return undefined;
+            }
+        }
+    }
+
+    if (propertyMetadata.ctor) {
+        return propertyMetadata.ctor(source[propertyName]);
+    }
+    else if (!isPrimitive(type)) {
+        return deserialize(type, source[propertyName]);
+    }
+    else {
+        return source[propertyName];
+    }
+};
+
+const getType = (target: any, propertyKey: string): any =>
+    Reflect.getOwnMetadata("design:type", Object.getPrototypeOf(target), propertyKey);
---
+++ src/json-property.ts
@@ -0,0 +1,17 @@
+import { JsonMetadata, METADATA_KEY } from './json-metadata';
+
+/**
+ * Attribute specifying a property to be serialized
+ * @param nameOrMetadata The name of the json property or metadata describing how to construct the property
+ */
+export function jsonProperty<T>(nameOrMetadata?: string | JsonMetadata<T>) {
+    let metadata: JsonMetadata<T> = typeof(nameOrMetadata) === 'string' ? { name: nameOrMetadata } : nameOrMetadata;
+
+    if (!!metadata.type && !!metadata.ctor) {
+        throw "Only one of type or ctor can be specified";
+    }
+
+    return function (target: any, propertyKey: string) {
+        Reflect.defineMetadata(METADATA_KEY, metadata, target, propertyKey);
+    };
+}
