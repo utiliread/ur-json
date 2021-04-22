127da8937b40468142765442b9e8b64b637b22a6
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2021-04-22 11:21:23 +0200

    Allow to override DateTime.fromISO options

--- src/converters/date-time-converter.ts
+++ src/converters/date-time-converter.ts
@@ -1,9 +1,15 @@
-import { DateTime } from "luxon";
+import { DateTime, DateTimeOptions } from "luxon";
 import { JsonConverter } from "../json-converter";
 
 class DateTimeConverter implements JsonConverter {
+    public options: DateTimeOptions;
+
+    constructor(options?: DateTimeOptions) {
+        this.options = options ?? { setZone: true };
+    }
+
     fromJson(source: any) {
-        return DateTime.fromISO(source, { setZone: true });
+        return DateTime.fromISO(source, this.options);
     }
 }
 
