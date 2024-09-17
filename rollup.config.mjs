847e3d7776d446d474089ebbd3c7f7a114304f7a
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2024-09-17 11:49:17 +0200

    Use rollup instead of parcel

---
+++ rollup.config.mjs
@@ -0,0 +1,37 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+/** @type {import('rollup').RollupOptions} */
+const options = {
+  input: "src/index.ts",
+  external: [
+    "@utiliread/http",
+    "@utiliread/http/json",
+    "@utiliread/json",
+    "luxon",
+  ],
+  plugins: [nodeResolve({ resolveOnly: ["base64-arraybuffer"] }), typescript()],
+};
+
+export default [
+  Object.assign(
+    {
+      output: {
+        file: "dist/index.mjs",
+        sourcemap: true,
+        format: "es",
+      },
+    },
+    options,
+  ),
+  Object.assign(
+    {
+      output: {
+        file: "dist/index.js",
+        sourcemap: true,
+        format: "cjs",
+      },
+    },
+    options,
+  ),
+];
